b7fd33f4aaa96fcd76ee1a58b2e606acfccb2d87
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-05-20 17:36:32 +0200

    leayout terminato

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,11 +1,18 @@
-<img src="{{$comic->thumb}}" alt="">
-<h1>{{$comic->title}}</h1>
-<div class="text">
-    <p>{{$comic->description}}</p>
-    <div class="details">
-        <span>${{$comic->price}}</span>
-        <span>{{$comic->series}}</span>
-        <span>{{$comic->sale_date}}</span>
-        <span>{{$comic->type}}</span>
+
+@extends('layouts.app')
+
+@section('main')
+    <div class="comic">
+        <img src="{{$comic->thumb}}" alt="">
+        <h1>{{$comic->title}}</h1>
+        <div class="text">
+            <p>{{$comic->description}}</p>
+            <div class="details">
+                <span class='price'>${{$comic->price}}</span>
+                <span>{{$comic->series}}</span>
+                <span>{{$comic->sale_date}}</span>
+                <span>{{$comic->type}}</span>
+            </div>
+        </div>
     </div>
-</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,31 +64,17 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    See all Comics
-                </div>
+        <div class="content">
+            <div class="title m-b-md">
+                See all Comics
+            </div>
 
-                <div class="links">
-                    <a href="{{route('comics.index')}}">Comics</a>
-                    <a href="{{route('comics.create')}}">Add Comic</a>
+            <div class="links">
+                <a href="{{route('comics.index')}}">Comics</a>
+                <a href="{{route('comics.create')}}">Add Comic</a>
 
-                </div>
             </div>
         </div>
     </body>
